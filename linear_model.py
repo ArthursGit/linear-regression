16501f24692b35929645fe0d3a3ca792ab8a868d
Artur Galstyan <galstyan.artu@gmail.com>
2022-03-04 21:38:20 +0100

    finished linear regression

--- linear_model.py
+++ linear_model.py
@@ -3,6 +3,7 @@ import sklearn.datasets as ds
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class LinearModel:
 
     def __init__(self, n_features):
@@ -11,18 +12,44 @@ class LinearModel:
         )
 
     def predict(self, data):
-        data = torch.tensor(data)
+        if not torch.is_tensor(data): 
+            data = torch.tensor(data)
         assert data.shape[1] == self.theta.shape[0], \
             f"Shape mismatch, expected {self.theta.shape[0]}, got {data.shape[1]}"
         return data @ self.theta
 
+    def measure_cost(self, training_data, targets):
+        n_samples = len(training_data)
+        predictions = self.predict(training_data)
+        return ((predictions - targets) ** 2).sum() / (2 * n_samples)
 
-def get_synthetic_data(n_features=1, n_informative=1, noise=5, coefficients=True, random_state=42):
+    def train(self, training_data, targets, learning_rate=0.01, n_episodes=500, batch_size=64):
+        n_samples = len(training_data)
+        for episode in range(n_episodes):
+            i = 0
+            while i < n_samples:
+                if i + batch_size > n_samples:
+                    batch_size = n_samples - i
+
+                batch_x = training_data[i:i + batch_size]
+                batch_y = targets[i:i+batch_size]
+
+                prediction = self.predict(batch_x)
+                self.theta -= learning_rate * ((batch_x.T @ (prediction - batch_y)) / n_samples)
+
+                i += batch_size
+
+            cost = self.measure_cost(training_data, targets)
+
+            print(f"Episode {episode}, Cost = {cost}")
+
+
+def get_synthetic_data(n_features=1, n_informative=1, noise=20, coefficients=True, random_state=42):
     X, y, coef = ds.make_regression(n_features=n_features,
-                              n_informative=n_informative,
-                              noise=noise,
-                              coef=coefficients,
-                              random_state=random_state)
+                                    n_informative=n_informative,
+                                    noise=noise,
+                                    coef=coefficients,
+                                    random_state=random_state)
 
     X = np.c_[np.ones((len(X), 1)), X]
 
@@ -31,11 +58,25 @@ def get_synthetic_data(n_features=1, n_informative=1, noise=5, coefficients=True
 
 training_data, target, coefficients = get_synthetic_data()
 
+training_data_tensor = torch.tensor(training_data)
+target_tensor = torch.tensor(target).unsqueeze(1)
 
 model = LinearModel(2)
 
-prediction = model.predict(training_data)
+model.train(training_data_tensor, target_tensor)
 
-print(prediction)
+print(coefficients)
+print(model.theta)
 
 
+plt.scatter(training_data[:, 1], target)
+
+x = np.linspace(-3, 3, 2)
+x = np.c_[np.ones((len(x), 1)), x]
+x = torch.tensor(x)
+
+predictions = model.predict(x)
+
+plt.plot(x[:, 1], predictions, color='red')
+
+plt.show()
